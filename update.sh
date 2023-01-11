649034d7a70f94e0b1a11ff2a36c7ab462669057
natejank <njj3397@rit.edu>
2023-01-10 20:51:23 -0500

    track vscode settings

--- update.sh
+++ update.sh
@@ -9,3 +9,14 @@ cp $HOME/.zshrc zshrc
 # ncspot
 mkdir ncspot 2> /dev/null
 cp $HOME/.config/ncspot/config.toml ncspot/config.toml
+
+# vscode
+case `uname -s` in
+    'Darwin')
+        vscode_settings="$HOME/Library/Application Support/Code/User/settings.json"
+        ;;
+    *)
+        echo 'Unsupported os! Where is your vscode settings.json?'
+        ;;
+esac
+cp "$vscode_settings" vscode_settings.json
