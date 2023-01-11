1e647f5e44c091c31c51f5a2dd2f72c9f702c2ea
natejank <njj3397@rit.edu>
2023-01-11 14:56:59 -0500

    improve update script error handling/flexibility; log vscode extensions
    - added install_vscode_extensions.sh

---
+++ install_vscode_extensions.sh
@@ -0,0 +1,6 @@
+# Script to reinstall vscode extensions automatically
+code --install-extension kokororin.vscode-phpfmt
+code --install-extension ms-python.isort
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension vscodevim.vim
--- update.sh
+++ update.sh
@@ -1,22 +1,49 @@
 #!/usr/bin/env bash
 
 # vim
-cp $HOME/.vimrc vimrc
+system_vimrc="$HOME/.vimrc"
+VIMRC="vimrc"
+
+cp "$system_vimrc" "$VIMRC" &> /dev/null
 
 # zsh
-cp $HOME/.zshrc zshrc
+system_zshrc="$HOME/.zshrc"
+ZSHRC="zshrc"
+
+cp "$system_zshrc" "$ZSHRC" &> /dev/null
 
 # ncspot
-mkdir ncspot 2> /dev/null
-cp $HOME/.config/ncspot/config.toml ncspot/config.toml
+system_ncspot="$HOME/.config/ncspot/config.toml"
+NCSPOT_DIR="ncspot"
+NCSPOT_CFG="$NCSPOT_DIR/config.toml"
+
+mkdir "$NCSPOT_DIR" &> /dev/null
+cp "$system_ncspot" "$NCSPOT_CFG" &> /dev/null
 
 # vscode
+VSCODE_SETTINGS='vscode_settings.json'
+VSCODE_EXTENSIONS='install_vscode_extensions.sh'
+
+# find settings.json
 case `uname -s` in
     'Darwin')
-        vscode_settings="$HOME/Library/Application Support/Code/User/settings.json"
+        system_vscode_settings="$HOME/Library/Application Support/Code/User/settings.json"
         ;;
     *)
         echo 'Unsupported os! Where is your vscode settings.json?'
         ;;
 esac
-cp "$vscode_settings" vscode_settings.json
+
+cp "$system_vscode_settings" "$VSCODE_SETTINGS" &> /dev/null
+
+# generate script to install vscode extensions
+# this allows for easier migration, and prevents typosquatting attacks
+# by ensuring I am reinstalling the exact same extension as before.
+if command -v code &> /dev/null; then
+    rm "$VSCODE_EXTENSIONS" &> /dev/null
+    echo "# Script to reinstall vscode extensions automatically" > "$VSCODE_EXTENSIONS"
+    for ext in `code --list-extensions`; do
+        echo "code --install-extension $ext" >> "$VSCODE_EXTENSIONS"
+    done
+    chmod +x "$VSCODE_EXTENSIONS" &> /dev/null
+fi
