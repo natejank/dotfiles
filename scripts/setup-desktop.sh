47973e4ed7d1a8ea8ca0798a41dca6bec92e6517
natejank <njj3397@rit.edu>
2025-06-05 12:58:58 -0400

    stuff for gnome-keyring to work

--- scripts/setup-desktop.sh
+++ scripts/setup-desktop.sh
@@ -22,7 +22,7 @@ ensure_installed_nir mpv
 # screen recording
 ensure_installed grimshot wf-recorder 
 # utilities required for sway
-ensure_installed wlsunset wev mako-notifier fuzzel brightnessctl wl-mirror dex playerctl libnotify-bin wl-clipboard libglib2.0-bin libsecret-tools adwaita-icon-theme-legacy
+ensure_installed wlsunset wev mako-notifier fuzzel brightnessctl wl-mirror dex playerctl libnotify-bin wl-clipboard libglib2.0-bin libsecret-tools adwaita-icon-theme-legacy ssh-askpass-gnome
 # status bar
 ensure_installed waybar gir1.2-playerctl-2.0 libappindicator3-1
 # settings apps
@@ -33,3 +33,6 @@ sudo ufw enable
 # configure nemo for sway
 gsettings set org.nemo.desktop show-desktop-icons false
 gsettings set org.cinnamon.desktop.default-applications.terminal exec alacritty
+
+echo "Note: You must configure PAM for gnome-keyring to work properly. I didn't include this in the script because having a script mess with the pam config seems like a bad idea."
+echo "See https://wiki.archlinux.org/title/GNOME/Keyring#PAM_step; place the edits in /etc/pam.d/common-{auth,session} instead of where the AW says"
